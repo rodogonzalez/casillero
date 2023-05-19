8d3bcfd4668d401efc7fb450238001e425e53680
rodolfo <rodogonzalez@msn.com>
2023-05-19 09:29:04 -0600

    layout improvements

--- laravel/resources/views/device_user_view.blade.php
+++ laravel/resources/views/device_user_view.blade.php
@@ -1,21 +1,25 @@
 @extends(backpack_view('blank'))
 @section('content')
-    <span>{{$device->name}}</span>
+    <span>{{ $device->name }}</span>
     <h1>Lockers Available</h1>
-    <div class="row">
-    @foreach ($lockers as $locker_box)        
-        <div class='col-4 locker_skin'>            
-            @foreach ($locker_box as $locker_port =>$data)                
-                @if ($data['status']=="available")
-                <a href="{{$data['link']}}" class="btn button">
-                      [ {{$data['caption']}} {{$data['status']}} ]                    
-                </a>
-                @else
-                    [ {{$data['caption']}} {{$data['status']}} ]                
-                @endif
+    <div class="lockers-container">
+
+        @foreach ($lockers as $locker_box)
+            @foreach ($locker_box as $locker_port => $data)
+                <div class='locker {{ $data['status'] }}'>
+                    {{ $data['caption'] }}
+                    @if ($data['status'] == 'available')
+                        <a href="{{ $data['link'] }}" class="btn button">
+                            <i class="la la-unlock"></i>[Open]
+                        </a>
+                    @else
+                        <i class="la la-lock"></i> - locked
+                    @endif
+                    <br>{{ $data['status'] }}
+                </div>
             @endforeach
-        </div>
-    @endforeach
+        @endforeach
+
 
     </div>
 @endsection
