b4b26b903186864d7be890ffedfac83d7b2b940c
rodolfo <rodogonzalez@msn.com>
2023-05-19 18:50:50 -0600

    update

--- laravel/lang/en/messages.php
+++ laravel/lang/en/messages.php
@@ -7,7 +7,7 @@ return [
     'thankyou'         => 'Gracias! , retire sus objetos y cierre el locker',
     'use_the_qr'       => 'Utilice este codigo para abrir su locker cuando se le solicite',
     'scan.camera'      => 'Scannear - leer con la camara',
-    'senter.code'      => 'Ingrese el código',
+    'enter.code'      => 'Ingrese el código',
     'available'        => 'Disponible',
     'out-of-service'   => 'Fuera de Servicio',
     'in-use'           => 'Ocupado',
