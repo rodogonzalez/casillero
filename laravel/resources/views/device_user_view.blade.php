220a36e8ce3847076a05830c41a035f118be338f
rodolfo <rodogonzalez@msn.com>
2023-05-22 08:56:41 -0600

    update

--- laravel/resources/views/device_user_view.blade.php
+++ laravel/resources/views/device_user_view.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <span>{{ $device->name }}</span>
     <h1>{{ __('messages.devicedash.title') }}</h1>
-    
-    
+
+
     <div class="lockers-container">
 
         @foreach ($lockers as $locker_box)
@@ -18,12 +18,20 @@
                     @else
                         <i class="la la-lock"></i> <a href="/open">[ {{ __('messages.open') }} ]</a>
                     @endif
-                    <div class="status">{{ __('messages.' .$data['status'] ) }} </div>
-                    
+                    <div class="status">{{ __('messages.' . $data['status']) }} </div>
+
                 </div>
             @endforeach
         @endforeach
 
 
     </div>
+    @if (env('ADSENSE_ENABLED', false))
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3258144106657369" data-ad-slot="8812111908"
+            data-ad-format="auto" data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    @endif
 @endsection
