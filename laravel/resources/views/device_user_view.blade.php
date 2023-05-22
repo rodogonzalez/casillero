fea5f1c7479697cba8be632de0283e05c115e647
rodolfo <rodogonzalez@msn.com>
2023-05-22 12:58:16 -0600

    update

--- laravel/resources/views/device_user_view.blade.php
+++ laravel/resources/views/device_user_view.blade.php
@@ -26,12 +26,5 @@
 
 
     </div>
-    @if (env('ADSENSE_ENABLED', false))
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3258144106657369" data-ad-slot="8812111908"
-            data-ad-format="auto" data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    @endif
+   
 @endsection
